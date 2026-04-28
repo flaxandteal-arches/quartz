5985ae0f72a952a21dd20023803b14ec89aa18c7
Stuart Marshall <stuart.marshall@flaxandteal.co.uk>
2026-04-28 08:57:31 +0100

    chore: replace upload files patch that was moved to app

--- quartz/apps.py
+++ quartz/apps.py
@@ -14,3 +14,26 @@ class QuartzConfig(AppConfig):
 
         user_logged_in.connect(ensure_user_backend)
 
+    def _patch_filelist_path_join():
+        """
+        Upstream FileListDataType.transform_value_for_tile builds a blob key via
+        `"%s/%s" % (settings.UPLOADED_FILES_DIR, name)`, which yields a leading slash
+        (e.g. `/foo.jpg`) when UPLOADED_FILES_DIR="". Every other call site in arches
+        uses os.path.join. Patch to match.
+        """
+        import inspect
+        import textwrap
+
+        from arches.app.datatypes import datatypes as arches_datatypes
+
+        method = arches_datatypes.FileListDataType.transform_value_for_tile
+        src = textwrap.dedent(inspect.getsource(method))
+        old = '"%s/%s" % (settings.UPLOADED_FILES_DIR, str(tile_file["name"]))'
+        new = 'os.path.join(settings.UPLOADED_FILES_DIR, str(tile_file["name"]))'
+        if old not in src:
+            return
+        namespace = arches_datatypes.__dict__.copy()
+        exec(src.replace(old, new), namespace)
+        arches_datatypes.FileListDataType.transform_value_for_tile = namespace[
+            "transform_value_for_tile"
+        ]
