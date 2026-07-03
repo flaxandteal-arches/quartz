f135b31f3973051320a5e28bfecd6b920daaafdc
Andrea K. <andrea.kyurchiev@flaxandteal.co.uk>
2026-07-03 13:11:20 +0300

    chore: remove coral specific fields from assessments

--- quartz/media/js/views/components/reports/scenes/assessments.js
+++ quartz/media/js/views/components/reports/scenes/assessments.js
@@ -23,21 +23,8 @@ export default ko.components.register(
                 columns: Array(5).fill(null),
             };
 
-            self.issueReportsTableConfig = {
-                ...this.defaultTableConfig,
-                columns: [
-                    { width: "20%" },
-                    { width: "20%" },
-                    { width: "20%" },
-                    { width: "20%" },
-                    { width: "20%" },
-                    null,
-                ],
-            };
-
             self.dataConfig = {
                 scientificDate: "scientific date assignment",
-                issueReports: "issue report",
                 artefactConditin: undefined,
             };
 
@@ -48,16 +35,13 @@ export default ko.components.register(
             self.add = params.addTile || self.addNewTile;
             self.scientificDate = ko.observable();
             self.artefactCondition = ko.observableArray();
-            self.issueReports = ko.observableArray();
             self.visible = {
                 scientificDate: ko.observable(true),
                 artefactCondition: ko.observable(true),
-                issueReports: ko.observable(true),
             };
             Object.assign(self.dataConfig, params.dataConfig || {});
 
-            if (params?.compiled) {
-            } else {
+            if (!params?.compiled) {
                 const scientificDate = self.getRawNodeValue(
                     params.data(),
                     self.dataConfig.scientificDate
@@ -95,32 +79,6 @@ export default ko.components.register(
                             return { type, file, endDate, startDate, tileid };
                         })
                     );
-                }
-
-                const issueReportsData = self.getRawNodeValue(params.data(), {
-                    testPaths: [["issue report"]],
-                });
-                if (issueReportsData) {
-                    issueReportsData.forEach((report) => {
-                        const issueReport = {};
-                        issueReport.componentDamageType = self.getNodeValue(report, "component damage type");
-                        issueReport.damageType = self.getNodeValue(report, "damage type");
-                        issueReport.notes = self.getNodeValue(report, "issue notes", "issue note");
-                        issueReport.proposalActionType = self.getNodeValue(report, "issue proposal", "action type");
-                        issueReport.proposalApprover = self.getNodeValue(report, "issue proposal", "approver", "approved by");
-                        issueReport.proposalContactDetails = self.getNodeValue(report, "issue proposal", "contact details", "contact details value");
-                        issueReport.proposalIntendedDateStart = self.getNodeValue(report, "issue proposal", "intended dates", "intended start date");
-                        issueReport.proposalIntendedDateEnd = self.getNodeValue(report, "issue proposal", "intended dates", "intended end date");
-                        issueReport.proposalDate = self.getNodeValue(report, "issue proposal", "proposal date", "proposal date value");
-                        issueReport.proposalDescriptionType = self.getNodeValue(report, "issue proposal", "proposal description type", "@display_value");
-                        issueReport.proposalText = self.getNodeValue(report, "issue proposal", "proposal text");
-                        issueReport.reference = self.getNodeValue(report, "issue reference", "reference number");
-                        issueReport.materialFabricDamage = self.getNodeValue(report, "material/fabric damage type");
-                        issueReport.signOffDate = self.getNodeValue(report, "sign off date", "sign off date value");
-                        issueReport.statusType = self.getNodeValue(report, "status type");
-                        issueReport.workFinishDate = self.getNodeValue(report, "work finish date", "work finish date value");
-                        self.issueReports.push(issueReport);
-                    });
                 }
             }
         },
